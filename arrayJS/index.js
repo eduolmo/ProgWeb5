f5633522502fe2845d690484c6c6c8abfaee4698
Eduardo Olmo <91472785+eduolmo@users.noreply.github.com>
2023-03-13 12:53:35 -0300

    Não fiz a última atividade

---
+++ arrayJS/index.js
@@ -0,0 +1,28 @@
+let cores = ['azul','vermelho','amarelo','verde','laranja','roxo','rosa']
+let imprimir = document.getElementById('imprimir')
+let div = document.getElementById('cores')
+
+imprimir.addEventListener('click', () => {
+    div.innerHTML = ''
+    for(i in cores){
+        div.innerHTML += `Minha cor ${i} é ${cores[i]} <br>`
+    }
+})
+//
+let num = [1,2,3,4,5]
+let elevar = document.getElementById('elevar')
+let quadrado = document.getElementById('quadrado')
+
+elevar.addEventListener('click', () => {
+    quadrado.innerHTML = num.map(function(n){return n**2})
+})
+//
+let numeros = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+let converter = document.getElementById('converter')
+let algarismos = document.getElementById('algarismos')
+/*
+converter.addEventListener('click', () => {
+
+})
+*/
+
