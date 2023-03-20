e51d6b5a1e4c4b1e94ed34842c3e612ca5d24cc0
Eduardo Olmo <91472785+eduolmo@users.noreply.github.com>
2023-03-20 10:40:19 -0300

    Acho que terminei kkk
    O ultimo dever tá dando um bug que não faz sentido. Quando eu utilizo 'break' funciona, porém utilizar ou não o 'break' não deveria fazer diferença

--- arrayJS/index.js
+++ arrayJS/index.js
@@ -8,7 +8,9 @@ imprimir.addEventListener('click', () => {
         div.innerHTML += `Minha cor ${i} é ${cores[i]} <br>`
     }
 })
+
 //
+
 let num = [1,2,3,4,5]
 let elevar = document.getElementById('elevar')
 let quadrado = document.getElementById('quadrado')
@@ -16,13 +18,23 @@ let quadrado = document.getElementById('quadrado')
 elevar.addEventListener('click', () => {
     quadrado.innerHTML = num.map(function(n){return n**2})
 })
+
 //
+
 let numeros = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+let romanos = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I']
+
 let converter = document.getElementById('converter')
 let algarismos = document.getElementById('algarismos')
-/*
-converter.addEventListener('click', () => {
 
+converter.addEventListener('click', () => {
+    let x = document.getElementById('num').value
+    
+    for(i = 0; numeros.length; i++){
+        if(x == numeros[i]){
+            algarismos.innerHTML = romanos[i]
+        }
+    }
 })
-*/
+
 
