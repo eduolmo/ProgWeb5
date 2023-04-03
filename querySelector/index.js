55fe249b56bc109e38bea7f423aa1beceaf8af84
Eduardo Olmo <91472785+eduolmo@users.noreply.github.com>
2023-04-03 11:52:10 -0300

    Ainda não terminei a atividade 9
    Preciso mudar o display do 'conteudo' quando eu clico no 'cabecalho'. Acho que vou precisar adicionar manualmente para cada item do 'cabecalho' a função de mudar o display dos item correspondente no 'conteudo'.

---
+++ querySelector/index.js
@@ -0,0 +1,25 @@
+let cabecalho = document.querySelectorAll('.accordion-cabecalho')
+let conteudo = document.querySelectorAll('.accordion-conteudo')
+
+
+
+
+/*
+for(i = 0; i < cabecalho.length; i++){
+
+    cabecalho[i].addEventListener('click',() => {
+        console.log('add+')
+        console.log(conteudo[i])
+        if(conteudo[i].style.display == 'none'){
+            conteudo[i].style.display = 'flex'
+        }
+        else{
+            conteudo[i].style.display = 'none'
+        }
+    })
+
+}
+*/
+
+
+
